1a0a6b109d2399b9cb22699e05e7829eb57dd43d
Jessica Honorato Mauer <33338932+mauerjh@users.noreply.github.com>
2020-06-04 22:33:08 -0300

    Update topmed_imputation.sh

--- topmed_imputation.sh
+++ topmed_imputation.sh
@@ -1,16 +1,17 @@
-plink2 --bfile INPD_todos_qc --maf 0.01 --geno 0.1 --mind 0.1 --hwe 0.000001 --recode-vcf --out INPD_todos
-#Separar os arquivos por cromossomos (chr1-chr22)
-for i in `seq 1 22`; do plink2 --vcf INPD_todos.vcf --chr ${i} --recode-vcf --out chr${i}_INPD; done
-
+plink2 --bfile arquivo --make-pgen --out arquivo_preimput
+plink2 --pfile ../INPD_todos_semhet_final --maf 0.01 --geno 0.1 --mind 0.1 --hwe 0.000001 --export vcf --out INPD_todos
+for i in `seq 1 22`; do plink2 --vcf INPD_todos.vcf --chr ${i} --export vcf --out chr${i}_INPD; done
 for i in `seq 1 22`; do vcf-sort chr${i}_INPD.vcf | bgzip -c > chr${i}_INPD_sort1.vcf.gz; done
-
-for i in '1 22'; do bcftools index chr${i}_INPD_sort1.vcf.gz ; done
-
-for i in 'seq 1 22'; do bcftools norm -cx chr${i}_INPD_sort1.vcf.gz -f human_g1k_v37.fasta -Oz chr${i}_INPD_TOPMED_ref.vcf.gz; done
-
-#Create a sorted *.vcf.gz file using VCFtools and tabix (including bgzip):
+for i in `seq 1 22`; do ../../plink2 --vcf INPD_todos.vcf --chr ${i} --export vcf --out chr${i}_INPD; done
+for i in `seq 1 22`; do vcf-sort chr${i}_INPD.vcf | bgzip -c > chr${i}_INPD_sort1.vcf.gz; done
+for i in {1..22}; do echo bcftools index chr${i}_INPD_sort1.vcf.gz ; done >> indexar
+chmod 755 indexar
+./indexar
+for i in {1..22}; do echo bcftools norm -cx chr${i}_INPD_sort1.vcf.gz -f human_g1k_v37.fasta -o chr${i}_INPD_TOPMED_ref.vcf; done >> normalizar
+chmod 755 normalizar
+nohup ./normalizar &
 for i in `seq 1 22`; do vcf-sort chr${i}_INPD_TOPMED_ref.vcf| bgzip -c > chr${i}_INPD_fimTOPMED.vcf.gz; done
-
+ 
 ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/technical/reference/human_g1k_v37.fasta.fai
 
 #########################
@@ -57,29 +58,7 @@ chmod 755 ./filterCHR9-22
 nohup ./filterCHR9-22 &
 
 #juntando arquivos
-bcftools concat -Oz 1filterinfo.vcf.gz 2filterinfo.vcf.gz 3filterinfo.vcf.gz 4filterinfo.vcf.gz 5filterinfo.vcf.gz 6filterinfo.vcf.gz 7filterinfo.vcf.gz 8filterinfo.vcf.gz 9filterinfo.vcf.gz 10filterinfo.vcf.gz 11filterinfo.vcf.gz 12filterinfo.vcf.gz 13filterinfo.vcf.gz 14filterinfo.vcf.gz 15filterinfo.vcf.gz 16filterinfo.vcf.gz 17filterinfo.vcf.gz 18filterinfo.vcf.gz 19filterinfo.vcf.gz 20filterinfo.vcf.gz 21filterinfo.vcf.gz 22filterinfo.vcf.gz -o imp_bruta.vcf.gz
-
-#### Merge results GWAS with imputed information file and filter by R2 >= 6 (the results have been filtered by MAF before)
-# Extract columns of interest in GWAS results: CHR POS SNPID Allele1 Allele2 AF_Allele2 N BETA SE Tstatp.value
-zcat AD.chr19.SAIGE.txt.gz | awk '{print
-$1,$2,$3,$4,$5,$7,$8,$9,$10,$11,$12}' > temp_AD.chr19.results.txt
-# Extract columns of interest in imputation information: SNP Rsq
-zcat ../saige/chr19.info.gz | awk '{print $1,$7}' > temp_chr19.info.txt
-# Merge information from the GWAS and R2
-awk 'NR==FNR{a[$1]=$2; next} $3 in a{print $0,a[$3]}' temp_chr19.info.txt temp_AD.chr19.results.txt > temp
-# Filer by R2 >= 0.6 and put the header
-echo 'CHR POS SNPID Allele1 Allele2 AF_Allele2 N BETA SE Tstat p.value
-Rsq' > AD.chr19.results.QC.txt
-awk '{if ($12>=0.6) print $0}' temp >> AD.chr19.results.QC.txt
-#### Plot the results: Manhattan plot and regional plot
-awk '{if (NR>1) print $1,$2,$11}' temp_AD.chr19.results.QC.txt >
-temp_plot.AD.chr19.txt
-sort -k11 -g temp_AD.chr19.results.QC.txt | head # lowest p-value at
-19:45422946 , p = 9.3515193823689e-16
-awk '{if (NR==1) print $1,$2,$3,$11; else if ($2>= 44922946 && $2<=
-45922946) print $1,$2,"chr"$3,$11}' AD.chr19.results.QC.txt >
-temp_ld.AD.chr19.txt
-
+bcftools concat -Oz 1filter.vcf.gz 2filter.vcf.gz 3filter.vcf.gz 4filter.vcf.gz 5filter.vcf.gz 6filter.vcf.gz 7filter.vcf.gz 8filter.vcf.gz 9filter.vcf.gz 10filter.vcf.gz 11filter.vcf.gz 12filter.vcf.gz 13filter.vcf.gz 14filter.vcf.gz 15filter.vcf.gz 16filter.vcf.gz 17filter.vcf.gz 18filter.vcf.gz 19filter.vcf.gz 20filter.vcf.gz 21filter.vcf.gz 22filter.vcf.gz -o imp_bruta.vcf.gz
 
 #Converter para plink2 format (pgen, pvar, psam)
 plink2 --vcf imp_bruta.vcf.gz --make-pgen --out imputacao_bruta
@@ -88,17 +67,16 @@ plink2 --vcf imp_bruta.vcf.gz --make-pgen --out imputacao_bruta
 plink2 --pfile imputacao_bruta --rm-dup exclude-all list --make-pgen --out imp_semduplicatas
 
 #Remover indels 
-plink2 --pfile imp_semduplicatas --snps-only just-acgt --make-pgen --out imp_semindels
+plink2 --pfile imp_semduplicatas --snps-only just-acgt --make-bed --out imp_semindels
 
 #Fazendo arquivo updatesex:
-awk '{print $2, $5}' arquivo_antes_de_imputar.fam > upd_sex.txt
-gedit upd_sex.txt
-#colocar cabeçalho: '#IID SEX', salvar
+awk '{print 0, $2, $5}' arquivo_antes_de_imputar.fam > upd_sex.txt
+paste upd_sex.txt imp_semindels.fam | awk '{print $1, $2, $6,$7.$3,$9}' > imputado_sex.fam
+cp imp_semindels.bed imputado_sex.bed
+cp imp_semindels.bim imputado_sex.bim
+r
 #Fazendo arquivo fenotipo
 awk '{print $2, $6}' arquivo_antes_de_imputar.fam > upd_pheno.txt
 
-#Atualizando fenotipo e sexo
-plink2 --pfile imp_semindels --update-sex upd_sex.txt --pheno upd_pheno.txt --make-pgen --out imputado_sex_pheno
-
 #QC
-plink2 --pfile imputado_sex_pheno --maf 0.01 --mind 0.1 --geno 0.1 --hwe 0.000001 --make-pgen --out imputado_QC
+plink2 --bfile imputado_sex --maf 0.01 --mind 0.1 --geno 0.1 --hwe 0.000001 --make-bed --out imputado_QC
